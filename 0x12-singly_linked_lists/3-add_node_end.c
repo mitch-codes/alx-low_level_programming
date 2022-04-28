e3d8a0f7fac33c23753eab58681063d5740d02c9
mitch codes <mitchkodes@gmail.com>
2022-04-28 11:32:45 -0700

    linked lists

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -22,14 +22,14 @@ list_t *add_node_end(list_t **head, const char *str)
 	{
 		return (0);
 	}
-	
+
 	new_node->str = strdup(str);
 	new_node->len = strlen(str);
 	new_node->next = NULL;
 
 	if (str == NULL)
 	{
-		free (new_node);
+		free(new_node);
 		return (NULL);
 	}
 
