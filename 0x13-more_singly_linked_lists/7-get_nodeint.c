8f1dc06c42bf92927cdeb8dc46a5a8a607a55dc0
mitch codes <mitchkodes@gmail.com>
2022-04-28 11:53:01 -0700

    linked lists

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -16,11 +16,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	for (j = 0; j < index; j++)
 	{
+		if (head == NULL)
+		{
+			return (0);
+		}
 		head = head->next;
-	}
-	if (head == NULL)
-	{
-	return;
 	}
 	return (head);
 }
