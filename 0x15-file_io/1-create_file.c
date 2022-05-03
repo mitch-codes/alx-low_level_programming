ccbddd8ede9b36277130a35d0541ad942e7f0d3b
mitch codes <mitchkodes@gmail.com>
2022-05-03 07:25:04 -0700

    file descriptors

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,8 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	fd = open(filename, O_WRONLY | O_APPEND);
+	/*O_APPEND*/
+	fd = open(filename, O_WRONLY);
 
 	if (fd < 0)
 	{
