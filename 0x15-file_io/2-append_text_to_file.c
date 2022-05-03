585e77561fe1584b7e3dd076bfaf1efa4529303b
mitch codes <mitchkodes@gmail.com>
2022-05-03 07:18:45 -0700

    file descriptors

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -7,7 +7,7 @@
 #include <stdlib.h>
 
 /**
- * create_file - create a file and its contents
+ * append_text_to_file - create a file and its contents
  *
  * @filename: a string of the file name
  * @text_content: the files content
@@ -16,12 +16,13 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
-	int count = strlen(text_content);
+	int count;
 
 	if (filename == NULL || text_content == NULL)
 	{
 		return (-1);
 	}
+	count = strlen(text_content);
 	fd = open(filename, O_WRONLY | O_APPEND);
 
 	if (fd < 0)
