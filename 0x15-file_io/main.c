0e2a3217c52a9ce4b3a1b40349b6df32d3f788c6
mitch-codes <88305630+mitch-codes@users.noreply.github.com>
2022-05-04 06:30:14 +0300

    test

--- 0x15-file_io/main.c
+++ 0x15-file_io/main.c
@@ -7,11 +7,18 @@
  *
  * Return: Always 0.
  */
-int main(void)
+int main(int ac, char **av)
 {
-    int res;
+    ssize_t n;
 
-    res = create_file("hi", "");
-    printf("-> %i)\n", res);
+    if (ac != 2)
+    {
+        dprintf(2, "Usage: %s filename\n", av[0]);
+        exit(1);
+    }
+    n = read_textfile(av[1], 114);
+    printf("\n(printed chars: %li)\n", n);
+    n = read_textfile(av[1], 1024);
+    printf("\n(printed chars: %li)\n", n);
     return (0);
 }
