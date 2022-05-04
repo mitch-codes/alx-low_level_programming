7cee0829a82be36e15842c834c29630b85e571a6
mitch-codes <88305630+mitch-codes@users.noreply.github.com>
2022-05-04 06:32:13 +0300

    change

--- 0x15-file_io/main.c
+++ 0x15-file_io/main.c
@@ -7,18 +7,13 @@
  *
  * Return: Always 0.
  */
-int main(int ac, char **av)
+int main(void)
 {
     ssize_t n;
-
-    if (ac != 2)
-    {
-        dprintf(2, "Usage: %s filename\n", av[0]);
-        exit(1);
-    }
-    n = read_textfile(av[1], 114);
+    
+    n = read_textfile("hi", 3);
     printf("\n(printed chars: %li)\n", n);
-    n = read_textfile(av[1], 1024);
+    n = read_textfile(hi, 2);
     printf("\n(printed chars: %li)\n", n);
     return (0);
 }
