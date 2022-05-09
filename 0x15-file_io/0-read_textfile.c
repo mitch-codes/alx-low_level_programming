159c141a979dfc4ae8d0198e6dec62559f38bd31
mitch codes <mitchkodes@gmail.com>
2022-05-08 22:42:58 -0700

    file creation

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,23 +15,26 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	int count, y;
+	int fd, count, y;
 	char *buff;
 
 	if (filename == NULL)
 	{
 		return (0);
 	}
-	if (letters < 1)
-	{
-		return (0);
-	}
+	/*if (letters < 1)*/
+	/*{*/
+	/*	return (0);*/
+	/*}*/
 	fd = open(filename, O_RDONLY);
 
 	if (fd < 0)
 	{
 		return (0);
+	}
+	if (fd < letters)
+	{
+		letters = fd;
 	}
 	buff = malloc(sizeof(char) * letters);
 	if (buff == NULL)
