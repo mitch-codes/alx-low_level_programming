33f4a2a3e0a2c8ce021968f296e8b7a455907d52
mitch codes <mitchkodes@gmail.com>
2022-06-09 17:42:54 -0700

    doubly linked

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+#include"lists.h"
+
+/**
+ * add_dnodeint - print number of elements of linked list
+ *
+ * @head: the head pointer
+ * @n: the value to be added
+ * Return: the number of elements in the linked list
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *temp = malloc(sizeof(dlistint_t));
+
+	temp->prev = NULL;
+	temp->next = NULL;
+	temp->n = n;
+
+	if (*head == NULL)
+	{
+		*head = temp;
+	}
+	else
+	{
+		(*head)->prev = temp;
+		temp->next = *head;
+		*head = temp;
+	}
+	return (temp);
+}
+
+
