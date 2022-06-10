f026207b46bfc41b8806cf6219738b551e2ff0cf
mitch codes <mitchkodes@gmail.com>
2022-06-09 17:05:42 -0700

    doubly linked

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+#include"lists.h"
+
+/**
+ * print_dlistint - print the values of adoubly linked list
+ *
+ * @h: the head pointer
+ * Return: the number of elements in the linked list
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+	while (h != NULL)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
+
+
