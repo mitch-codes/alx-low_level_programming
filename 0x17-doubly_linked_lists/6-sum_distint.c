655aec6ac48ee0ce4d5a024a807ac2e1fea7b139
mitch codes <mitchkodes@gmail.com>
2022-06-12 01:52:50 -0700

    higher level

---
+++ 0x17-doubly_linked_lists/6-sum_distint.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+#include"lists.h"
+
+/**
+ * sum_dlistint - get the sum of all data in the nodes
+ *
+ * @head: the head pointer
+ * Return: the sum of the node data
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	while (head != NULL)
+	{
+		sum = sum + head->n
+		head = head->next;
+	}
+	return (sum);
+}
+
+
