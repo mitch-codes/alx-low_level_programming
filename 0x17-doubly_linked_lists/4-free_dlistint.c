3b2926dac4200d35d463a066891529a12dd47639
mitch codes <mitchkodes@gmail.com>
2022-06-12 01:19:06 -0700

    higher level

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+#include"lists.h"
+
+/**
+ * free_dlistint - frees up memory allocated for nodes of the list
+ *
+ * @h: the head pointer
+ * Return: the number of elements in the linked list
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+	while (h != NULL)
+	{
+		temp = head->next;
+		free(head)
+		head = temp;
+	}
+}
+
+
