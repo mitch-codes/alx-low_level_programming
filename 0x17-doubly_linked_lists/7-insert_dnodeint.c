46777961020dfb2c4d67549508e5149d5a324afb
mitch codes <mitchkodes@gmail.com>
2022-06-12 02:57:14 -0700

    higher level

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -26,6 +26,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (*h == NULL)
 	{
+		if (idx > 0)
+		{
+			return (NULL);
+		}
 		*h = temp;
 		return (temp);
 	}
