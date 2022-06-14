d203f4f6f5cbf2d1218dc3e0b963d761d4a46e1b
mitch codes <mitchkodes@gmail.com>
2022-06-13 17:41:45 -0700

    higher level

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -31,7 +31,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	while (i < idx)
 	{
 		place = place->next;
-		i++
+		i++;
 	}
 	place->next->prev = temp;
 	temp->next = place->next;
