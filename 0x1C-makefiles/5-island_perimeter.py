2b192e868ce5202a603f25ce43fc2e00d116df0e
MITCHELL OTIENO MONDA <88305630+mitch-codes@users.noreply.github.com>
2023-08-22 13:44:21 +0300

    Update 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -15,18 +15,22 @@ def island_perimeter(grid):
         then return the total length
 
     """
-    y = 0
-    x = 0
-    
-    for row in range(1, len(grid) - 1):
-            for col in range(1, len(grid[row]) - 1):
-                if grid[row][col] == 1:
-                    if grid[row][col - 1] == 0:
-                        y += 1
-                    if grid[row][col + 1] == 0:
-                        y += 1
-                    if grid[row - 1][col] == 0:
-                        x += 1
-                    if grid[row + 1][col] == 0:
-                        x += 1
-    return x + y
+    hsize = len(grid)
+    wsize = len(grid[1])
+    total = 0
+
+    print("width is ", hsize, "width is", wsize)
+
+    for i in range(hsize):
+        for j in range(wsize):
+            if (grid[i][j] == 1):
+                if (grid[i][j-1] == 0 or grid[i][j-1] == None):
+                    total += 1
+                if (grid[i][j+1] == 0 or grid[i][j+1] == None):
+                    total += 1
+                if (grid[i-1][j] == 0 or grid[i-1][j] == None):
+                    total += 1
+                if (grid[i+1][j] == 0 or grid[i+1][j] == None):
+                    total += 1
+
+    return total
